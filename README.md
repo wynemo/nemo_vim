080430aeee5adb38da479a2d1cb172f6aecf3864
wynemo <ggdabin@gmail.com>
2012-05-04 09:52:35 +0000

    add install instruction for windows

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 ###nemo's vim config
 
 My own Vim configurations and plugins which I used.
+
+####windows
+go to folder`%USERPROFILE%`
+
+    cd /d %USERPROFILE%
+    REM set path=%path%;C:\git\bin
+    git clone git://github.com/wynemo/nemo_vim.git .vim
+    cd .vim
+    install bat
+
+install *Exuberant Ctags*(system environment)
+
---
+++ install.bat
@@ -0,0 +1,10 @@
+REM win7 required, download junction.exe at http://technet.microsoft.com/en-us/sysinternals/bb896768
+REM run as admin
+fsutil hardlink create %USERPROFILE%\_vimrc %USERPROFILE%\.vim\vimrc
+fsutil hardlink create %USERPROFILE%\_gvimrc %USERPROFILE%\.vim\win_gvimrc
+%USERPROFILE%/bin/junction.exe -q %USERPROFILE%\vimfiles %USERPROFILE%\.vim
+cd %USERPROFILE%\.vim
+REM set path=%path%;C:\git\bin
+git submodule update --init
+echo "Installation finished"
+pause
